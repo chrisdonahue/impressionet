7004f72724d846a06a7958ece70310fdaedb7bdd
Chris Donahue <christopherdonahue@gmail.com>
2017-03-03 11:31:25 -0800

    finished timit extraction script

--- extract_timit.py
+++ extract_timit.py
@@ -27,6 +27,11 @@ def mel40_analyzer():
         return np.array(feats)
     return analyzer
 
+def raw_analyzer():
+    def analyzer(samples):
+        return samples
+    return analyzer
+
 _TEST_SPEAKERS = set([
     'MDAB0', 'MWBT0', 'FELC0',
     'MTAS1', 'MWEW0', 'FPAS0',
@@ -42,43 +47,87 @@ def load_timit(
         timit_dir,
         analyzer,
         limit=None):
-    wrd_fps = glob.glob(os.path.join(timit_dir, '**/**/**/*.WRD'))
-    dataset_to_speakers = defaultdict(set)
-    for wrd_fp in wrd_fps:
-        path_data = wrd_fp.split(os.sep)[-3:]
-        dialect = path_data[0]
-        sex = path_data[1][:1]
-        speaker_id = path_data[1]
-        sentence_id = os.path.splitext(path_data[3])[0]
-        dataset_to_speakers[dataset].add(speaker_id)
-    print {k:len(v) for k, v in dataset_to_speakers.items()}
+    glob_fp = os.path.join(timit_dir, '**/**/**/*.PHN')
+    phn_fps = glob.glob(glob_fp)
+    test_speakers = set()
+    valid_speakers = set()
+    train_speakers = set()
+    shapes = []
+    num_sa = 0
+    train_set = []
+    valid_set = []
+    test_set = []
+    if limit:
+        phn_fps = phn_fps[:limit]
+    for phn_fp in tqdm(phn_fps):
+        path_data = phn_fp.split(os.sep)[-4:]
+        dataset = path_data[0].lower()
+        dialect = path_data[1]
+        sex = path_data[2][:1]
+        speaker_id = path_data[2]
+        utterance_name = os.path.splitext(os.path.split(phn_fp)[1])[0]
 
+        if 'SA' in utterance_name:
+            num_sa += 1
+            continue
 
-    limit = limit if limit else len(wrd_fps)
-    results = []
-    for wrd_fp in tqdm(wrd_fps[:limit]):
-        wav_fp = '{}.WAV'.format(os.path.splitext(wrd_fp)[0])
-        path_data = wrd_fp.split(os.sep)[-3:]
-        dialect = path_data[0]
-        sex = path_data[1][:1]
-        speaker_id = path_data[1][1:]
-        sentence_id = os.path.splitext(path_data[2])[0]
-        metadata = (dialect, sex, speaker_id, sentence_id)
+        # build test speakers
+        if dataset == 'test' and speaker_id not in _TEST_SPEAKERS:
+            dataset = 'valid'
 
+        # extract PHN
+        phoneme_list = []
+        with open(phn_fp, 'r') as f:
+            for l in f.read().strip().splitlines():
+                start, stop, phoneme = l.split()
+                phoneme_list.append((start, stop, phoneme))
+
+        # extract WAV
+        wav_fp = '{}.WAV'.format(os.path.splitext(phn_fp)[0])
         loader = MonoLoader(filename=wav_fp, sampleRate=16000)
-        utterance = loader()
+        utterance_feats = analyzer(loader())
+        assert utterance_feats.dtype == np.float32
+        shapes.append(utterance_feats.shape)
 
-        feats = analyzer(utterance)
-        print metadata
-        print feats.shape
-        results.append((metadata, feats))
+        # add to list
+        entry = ((dialect, sex, speaker_id, utterance_name), phoneme_list, utterance_feats)
+        if dataset == 'train':
+            train_set.append(entry)
+            train_speakers.add(speaker_id)
+        elif dataset == 'valid':
+            valid_set.append(entry)
+            valid_speakers.add(speaker_id)
+        elif dataset == 'test':
+            test_set.append(entry)
+            test_speakers.add(speaker_id)
 
-    return results
+    print np.mean(shapes, axis=0)
+
+    if limit is None:
+        assert num_sa == 1260
+        assert len(train_speakers) == 462
+        assert len(valid_speakers) == 144
+        assert len(test_speakers) == 24
+
+    return train_set, valid_set, test_set
 
 if __name__ == '__main__':
     import cPickle as pickle
 
-    timit_dir = '/media/cdonahue/bulk1/datasets/timit/TIMIT'
+    timit_dir = '/home/cdonahue/timit/TIMIT'
+    limit = 10
 
-    analyzer = mel40_analyzer()
-    timit_feats = load_timit(timit_dir, analyzer, limit=10)
+    tag = 'raw'
+    analyzer = raw_analyzer()
+    #tag = 'mel40'
+    #analyzer = mel40_analyzer()
+
+    train, valid, test = load_timit(timit_dir, analyzer, limit=limit)
+
+    if limit is None:
+        with open('timit_{}_train.pkl'.format(tag), 'wb') as f:
+            pickle.dump(train, f)
+        with open('timit_{}_valid.pkl'.format(tag), 'wb') as f:
+            pickle.dump(valid, f)
+        with open('timit_{}_test.pkl'.format(tag), 'wb') as f:
+            pickle.dump(test, f)
